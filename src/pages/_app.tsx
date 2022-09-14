b02d2cb363d196e00f7a045508b95d5e00f03352
illumination-k <illumination.k.27@gmail.com>
2022-09-14 11:19:54 +0900

    dprint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
 interface SessionAppProps extends AppProps {
   pageProps: {
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,26 +1,26 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
 
 if (!GOOGLE_CLIENT_ID) {
-    throw new Error("GOOGLE CLIENT ID do not set")
+  throw new Error("GOOGLE CLIENT ID do not set");
 }
 
 if (!GOOGLE_CLIENT_SECRET) {
-    throw new Error("GOOGLE CLIENT SECRET do not set")
+  throw new Error("GOOGLE CLIENT SECRET do not set");
 }
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId:  GOOGLE_CLIENT_ID,
+      clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
-})
+});
